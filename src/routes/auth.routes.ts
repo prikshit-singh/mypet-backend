570d91373c86a2b9e99c27d8d105ba0894c53866
prikshit <43513459+prikshit-singh@users.noreply.github.com>
2025-04-26 13:00:32 +0530

    get user

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controller';
+import { login, signup,getCurrentUser } from '../controllers/auth.controller';
+import authenticateToken from '../middleware/auth';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/me',authenticateToken, getCurrentUser);
 
 export default router;
