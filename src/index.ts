7a8a7df82d896b05bb82d7dfe369d63a4ea44f52
prikshit-singh <prikshitlatherlather@gmail.com>
2025-07-05 15:02:19 +0530

    request file size

--- src/index.ts
+++ src/index.ts
@@ -37,11 +37,16 @@ const io = new IOServer(server, {
 // ✅ Setup WebSocket
 setupSocket(io);
 
-
+app.use((req, res, next) => {
+  const contentLength = req.headers['content-length'];
+  console.log(`[${req.method}] ${req.originalUrl} - Content-Length: ${contentLength} bytes`);
+  next();
+});
 // Middlewares
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors())
+
 app.use('/api/petimages', express.static(path.join(__dirname, '../petimages')));
 app.use('/api/profiles', express.static(path.join(__dirname, '../profiles')));
 app.get('/api', (req: Request, res: Response) => {
