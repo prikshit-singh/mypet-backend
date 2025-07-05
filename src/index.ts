5335166a70cef6db9bab58934621ac6a948fe6b1
prikshit-singh <prikshitlatherlather@gmail.com>
2025-07-05 14:49:52 +0530

    api request limit

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const server = new HttpServer(app); // HTTP server
 const io = new IOServer(server, {
   cors: {
     origin: '*', // Or specify allowed origins
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST','PUT','PATCH'],
   },
 });
 
@@ -39,7 +39,8 @@ setupSocket(io);
 
 
 // Middlewares
-app.use(express.json()); // Parse JSON request bodies
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors())
 app.use('/api/petimages', express.static(path.join(__dirname, '../petimages')));
 app.use('/api/profiles', express.static(path.join(__dirname, '../profiles')));
